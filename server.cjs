03fb2754a58ef90f169d793a76652e2203a421b4
@paulywrath <127055825+paulywrath@users.noreply.github.com>
2024-07-31 08:24:39 -0400

    Create and render affirmations list

--- server.cjs
+++ server.cjs
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 
+const affirmations = [`I`, `am`, `so`, `cool`]
+
 app.use(express.static('dist'))
 
 app.get(`/`, (req, res, next) => {
   res.sendFile(`${__dirname}/index.html`);
+})
+
+app.get(`/api/v1/list`, (req, res, next) => {
+  res.send(affirmations);
 })
 
 const port = process.env.PORT || 3000;
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
+import { useState, useEffect } from "react";
+
 function App() {
+
+  const [affs, setAffs] = useState([]);
+
+  useEffect(()=>{
+    const getAffirmations = async() => {
+      const response = await fetch (`http://localhost:3000/api/v1/list`);
+      const result = await response.json()
+      setAffs(result);
+    }
+    getAffirmations();
+  },[])
 
   return (
     <>
      <h1>Frontend connected</h1>
+
+    <ul>
+      {
+        affs.map((aff) => {
+          return <li>{aff}</li>
+        })
+      }
+    </ul>
+
     </>
   )
 }
